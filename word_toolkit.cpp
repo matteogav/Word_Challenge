268bb23b22ca9c1a5a47d606958e4d14f2f6bc80
stopenheaven <stopenheaven@gmail.com>
2018-11-21 10:26:30 +0100

    mes_frequent en proces

--- word_toolkit.cpp
+++ word_toolkit.cpp
@@ -22,7 +22,7 @@ string anagrama_canonic(const string& s) throw(){
     //Pre: true
     //Post: retorna l'string ordenda canonicament
     string res;
-    string aux_string=s;                //copia de s que anem redueint
+    string aux_string=s;                //copia de s que anem reduint
     if (s.es_canonic()) res=s;
     else  {
 	    char aux;
@@ -53,5 +53,44 @@ string anagrama_canonic(const string& s) throw(){
     Si l'string excl inclou totes les lletres de la 'A' a la 'Z' es
     retorna el caràcter '\0', és a dir, el caràcter de codi ASCII 0. */
 char mes_frequent(const string& excl, const list<string>& L) throw(){
+    string abc="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char res;
+    if (abc==excl) res='\0';
+    else {
+        string aux_string;
+        list<pair<char,int> > aux_pair;
+        int i=0;                    //i contador llista
+        while (i<L.size()){
+            aux_string=L[i];
+            char aux_char;
+            while (!aux_string.empty()){
+                aux_char=aux_string[0];                     //mirar sempre el primer char
+                bool trobat=false;
+                int j=0;
+                while(j<aux_pair.size() and !trobat){
+                    if (aux_char==aux_pair[j].first()) trobat=true;
+                    else j++;
+                }                
+                if (!trobat) pair<char,int> nou_pair=(aux_char,1);
+                else aux_pair[j].second+=1;
+                aux_string.erase(aux_string.begin()+0);     //borrar primer char de l'string
+            }
+            i++;
+        }
+    }
 
 }
+
+/*
+si excl es igual a [A..Z] res='\0';
+sino
+    bucle agafant una paraula de la llista
+        bucle mirant totes les lletres de la paraula agafada
+            si pertany a la llista pair<char,int> (.fisrt) sumar-li 1 a (.second)
+            sino afegir a la llista pair<char,int>
+            seguent lletra
+        seguent paraula
+    mirar qui te el numero mes gran amb xivato bool si hi ha algu igual
+    si bool = true res='>';
+    sino res=pair.first;
+*/
