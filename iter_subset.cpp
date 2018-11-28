0dada43492551ee89c8485c6d28dba928cc45f6a
matteogav <matteogav@gmail.com>
2018-11-28 11:54:58 +0100

    iter_matt

--- iter_subset.cpp
+++ iter_subset.cpp
@@ -3,7 +3,9 @@
 /* Construeix un iterador sobre els subconjunts de k elements
      de {1, ..., n}; si k > n no hi ha res a recórrer. */
 iter_subset::iter_subset(nat n, nat k) throw(error){
-
+  for(int i=0; i<k; ++i){
+    if(i+1<n) _info[i]=i+1;
+  }
 }
 
 /* Tres grans. Constructor per còpia, operador d'assignació i destructor. */
@@ -26,6 +28,10 @@ bool iter_subset::end() const throw(){
 /* Operador de desreferència. Retorna el subconjunt apuntat per
      l'iterador; llança un error si l'iterador apunta al sentinella. */
 subset iter_subset::operator*() const throw(error){
+  if(_final){
+    throw IterSubsetIncorr;
+  }
+  return _info;
 }
 
 /* Operador de preincrement.
@@ -38,11 +44,14 @@ iter_subset::iter_subset& iter_subset::operator++() throw(){
      Avança l'iterador al següent subconjunt en la seqüència i retorna el seu valor
      previ; no es produeix l'avançament si l'iterador ja apuntava al sentinella. */
 iter_subset::iter_subset iter_subset::operator++(int) throw(){
-
+  iter_subset cp(*this);
+  ++(*this);
+  return cp;
 }
 
 /* Operadors relacionals. */
 bool iter_subset::operator==(const iter_subset& c) const throw(){
 }
 bool iter_subset::operator!=(const iter_subset& c) const throw(){
+  return not (*this == c)
 }
