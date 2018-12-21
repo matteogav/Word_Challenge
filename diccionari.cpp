591816a84e6daf84c9df3e8f5e7e5a9a46f8466f
stopenheaven <stopenheaven@gmail.com>
2018-12-21 20:43:04 +0100

    modificat per compilar

--- diccionari.cpp
+++ diccionari.cpp
@@ -1,34 +1,72 @@
 #include "diccionari.hpp"
 
+/* si m és NULL, el resultat és NULL; sinó,
+   el resultat apunta al primer node d'un arbre ternari
+   de nodes que són còpia de l'arbre apuntat per m */
+typename copia_nodes::node* diccionari::copia_nodes(node* m) throw(error){
+  node* n;
+    if (m == NULL) n = NULL;
+    else {
+        n = new node;
+        try {
+            n->_c = m->_c;
+            n->_esq = copia_nodes(m->_esq);
+            n->_dret = copia_nodes(m->_dret);
+            n->_cen = copia_nodes(m->_cen);
+        }
+        catch(error) {
+            delete n;
+            throw;
+        }
+    }
+    return n;
+}
+
+typename void esborra_nodes(node* m) throw(){
+    if (m != NULL) {
+        esborra_nodes(m->_esq);
+        esborra_nodes(m->_dret);
+        esborra_nodes(m->_cen)
+        delete m;
+    }
+}
+
 /* Construeix un diccionari que conté únicament una paraula:
     la paraula buida. */
 diccionari::diccionari() throw(error){
-
+    _arrel = NULL;
 }
 
 /* Tres grans. Constructor per còpia, operador d'assignació i destructor. */
 diccionari::diccionari(const diccionari& D) throw(error){
-
+    _arrel = copia_nodes(D._arrel);
 }
 diccionari& diccionari::operator=(const diccionari& D) throw(error){
-
+    if (this != &D) {
+        node* aux;
+        aux = copia_nodes(D._arrel);
+        esborra_nodes(_arrel);
+        _arrel = aux;
+    }
+    return (*this);
 }
 diccionari::~diccionari() throw(){
-
+    esborra_nodes(_arrel);
 }
 
 /* Afegeix la paraula p al diccionari; si la paraula p ja formava
     part del diccionari, l'operació no té cap efecte. */
 void diccionari::insereix(const string& p) throw(error){
-    string k2 = p /*+ especial<string>()*/;
-    _arrel = rinsereix(_arrel, 0, k2);
+    string p2 = p /*+ especial<string>()*/;
+    _arrel = rinsereix(_arrel, 0, p2);
 }
 
 /* Retorna el prefix més llarg de p que és una paraula que pertany
     al diccionari, o dit d'una forma més precisa, retorna la
     paraula més llarga del diccionari que és prefix de p. */
 string diccionari::prefix(const string& p) const throw(error){
-
+    string s;
+    return s;
 }
 
 /* Retorna la llista de paraules del diccionari que satisfan el
@@ -50,7 +88,8 @@ nat diccionari::num_pal() const throw(){
 }
 
 typename diccionari::node* diccionari::rconsulta (node* n, nat i, const string &k) throw(){
-
+    node *ss =new node;
+    return ss;
 }
 
 typename diccionari::node* diccionari::rinsereix (node* n, nat i, const string &k) throw(error){
@@ -71,10 +110,8 @@ typename diccionari::node* diccionari::rinsereix (node* n, nat i, const string &
     else{
         if (n->_c > k[i]) n->_esq = rinsereix(n->_esq, i, k);
         else if (n->_c < k[i]) n->_dret = rinsereix(n->_dret, i ,k);
-        else n->_cen = rinsereix(n->_cen, i+1, k);
+        else n->_cen = rinsereix(n->_cen, i+1, k);  //n->_c == k[i]
     }
     return n;
 }
-
-//typename char operator[](int i) throw(error);
 
