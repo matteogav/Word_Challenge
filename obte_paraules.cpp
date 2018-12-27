3090f6fd171395f3e1972e8ff98dd66c06b49b58
stopenheaven <stopenheaven@gmail.com>
2018-12-27 20:15:16 +0100

    obte_paraules acabat falta comprovar jocs de prova

--- obte_paraules.cpp
+++ obte_paraules.cpp
@@ -3,8 +3,23 @@
 /* Retorna la llista de paraules que es poden formar usant k lletres de la paraula s. Llança error si k és major que
     la longitud de l'string s o k < 3. */
 void obte_paraules::obte_paraules(nat k, const string& s, const anagrames& A, list<string>& paraules) throw(error){
-  if(s.size() > k and k > 3){
-
+  if(s.size() > k and k > 3){                       //Llença error si k és menor que la longitud de s o k < 3.
+    list<string> aux_paraules;
+    A.mateix_anagrama_canonic(s, aux_paraules);     //Cridem funcio de classe anagrames per treure tots els anagrames
+                                                    //de la paraula s
+    bool trobat = false;
+    unsigned i=0;
+    string aux_p = aux_paraules.back();
+    list<string>::iterator it=aux_paraules.begin();
+    if (aux_p.size() >= k){                             //mirem ultim element si es de mida mes gran que k sino no fa res
+      while (i < aux_paraules.size() and !trobat){      //while d'afegir nomes les paraules de aux_paraules de mida k i sortir
+                                                        //del while si ja es mes gran de mida que k
+        string aux = *it;
+        if (aux.size() == k) paraules.push_back(aux);
+        else if (aux.size() > k) trobat = true;
+        i++;
+      }
+    }
   }
   else{
     throw LongitudInvalida;
@@ -15,8 +30,20 @@ void obte_paraules::obte_paraules(nat k, const string& s, const anagrames& A, li
     de menys a més longitud; a igual longitud les paraules estan en ordre alfabètic creixent.
     Llança un error si l'string s té menys de tres lletres. */
 void obte_paraules::obte_paraules(const string& s, const anagrames& A, list<string>& paraules) throw(error){
-  if(s.size() > 3){
-
+  if(s.size() > 3){                               //Llençar error si paraula es mes petit que 3
+    list<string> aux_paraules;
+    A.mateix_anagrama_canonic(s, aux_paraules);     //Cridem funcio de classe anagrames per treure tots els anagrames
+                                                    //de la paraula s
+    unsigned i=0;
+    string aux_p = aux_paraules.back();
+    list<string>::iterator it=aux_paraules.begin();
+    if (aux_p.size() >= 3){                       //mirem ultim element si es de mida mes gran que 3 sino no fa res
+      while (i < aux_paraules.size()){            //while d'afegir les paraules de aux_paraules de mida mes gran que 3
+        string aux = *it;
+        if (aux.size() >= 3) paraules.push_back(aux);
+        i++;
+      }
+    }
   }
   else{
     throw LongitudInvalida;
